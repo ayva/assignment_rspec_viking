d63a9ef2ceafcb78c2f1e39e1420dc6e7de1532c
Nick Sarlo <nicholas.l.sarlo@gmail.com>
2015-07-03 12:45:45 -0400

    double size and calls_some_method tests added

--- spec/warmup_spec.rb
+++ spec/warmup_spec.rb
@@ -10,11 +10,33 @@ describe Warmup do
     end
   end
 
-    describe "#double_size" do
+  describe "#double_size" do
     it 'returns doubled size of an array' do
-      my_double=double("double_size", :array => [1,1])
-      expect(my_double).to receive(:array).and_return(array.size * 2)
-      #expect(warmup.double_size([2,2,2])).to eq(6)
+      my_double=double("double", :size => 2)
+      expect(warmup.double_size(my_double)).to eq(4)
+    end
+  end
+
+  describe "calls_some_methods" do
+    it "receives the upcase method" do
+      str = "this is a string"
+      expect(str).to receive(:upcase!)
+      warmup.calls_some_methods(str)
+    end
+    it "receives the reverse method" do
+      str = "this is a string"
+      expect(str).to receive(:reverse!)
+      warmup.calls_some_methods(str)
+    end
+    it "changes the input string" do
+      str = "this is a string"
+      str_test = "this is a string"
+      warmup.calls_some_methods(str)
+      expect(str).to eq(str_test.upcase.reverse)
+    end
+    it "returns a different string than passed" do
+      str = "this is a string"
+      expect(warmup.calls_some_methods(str)).not_to eq(str)
     end
   end
 
