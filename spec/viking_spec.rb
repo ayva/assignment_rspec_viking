4bff139687355a16775e1df8ae5919778e6e4fc8
Nick Sarlo <nicholas.l.sarlo@gmail.com>
2015-07-03 14:32:06 -0400

    receive attack passed

--- spec/viking_spec.rb
+++ spec/viking_spec.rb
@@ -22,32 +22,54 @@ describe Viking do
     it "has a weapon that starts out nil by default" do
       expect(viking.weapon.nil?).to eq(true)
     end
+
   end
+
   describe "#pick_up_weapon" do
+
     it "sets a vikings weapon" do
       #weapon=instance_double("weapon", :is_a? => true)
       weapon=double( :is_a? => true)
       expect(viking.pick_up_weapon(weapon)).to eq(weapon)
-
     end
+
     it "raises an exception if applied to non-weapon object" do
       array=Array.new
       expect{viking.pick_up_weapon(array)}.to raise_error(RuntimeError)
     end
+
     it "replaces existing weapon" do
       axe = double( :is_a? => true)
       sword = double( :is_a? => true)
       steve = Viking.new("Steve", weapon = axe)
       expect(steve.pick_up_weapon(sword)).not_to eq(axe)
+    end
 
+  end
+
+  describe "#drop weapon" do
+
+    it "removes a weapon" do
+      steve = Viking.new(weapon = "axe")
+      expect(steve.drop_weapon).to be_nil
     end
   end
-  describe "#drop weapon" do
-    it "removes a weapon"
-  end
+
   describe "#receive attack" do
-    it "reduces health by specified amount"
-    it "calls take_damage method"
+
+    it "reduces health by specified amount" do
+      steve = Viking.new("Steve")
+      damage_dealt = 25
+      steve.receive_attack(damage_dealt)
+      expect(steve.health).to eq(75)
+    end
+
+    it "calls take_damage method" do
+      steve = Viking.new("Steve")
+      expect(steve).to receive(:take_damage)
+      steve.receive_attack(50)
+    end
+
   end
   describe "#attack" do
     it "causes the recipients health to drop"
